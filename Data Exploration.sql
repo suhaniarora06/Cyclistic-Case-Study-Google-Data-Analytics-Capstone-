a8e4a824f45e5fb293ba183234936bbe0bc1329e
suhaniarora06 <suhani9296@gmail.com>
2024-08-09 21:36:33 +0530

    Update Data Exploration.sql

--- Data Exploration.sql
+++ Data Exploration.sql
@@ -37,10 +37,17 @@ GROUP BY rideable_type;
 
 -- 7. Number of Riders with Annual Memberships using Classic, Docked, Electric Bikes 
 -- 8. Number of Casual Riders using Classic, Docked, Electric Bikes
-SELECT member_casual, rideable_type, COUNT(ride_id) AS number_of_riders,
-(COUNT(ride_id)/(SELECT CAST(COUNT(*) AS FLOAT) FROM [dbo].[all_tripdata_STAGING])) *100 AS percentage_total
-FROM [dbo].[all_tripdata_STAGING]
-GROUP BY member_casual, rideable_type;
+WITH TotalCounts AS (
+    SELECT member_casual, COUNT(ride_id) AS total_rides
+    FROM [dbo].[all_tripdata_STAGING]
+    GROUP BY member_casual
+)
+SELECT a.member_casual, a.rideable_type, COUNT(a.ride_id) AS number_of_riders,
+       (COUNT(a.ride_id) * 100.0 / t.total_rides) AS percentage_total
+FROM [dbo].[all_tripdata_STAGING] a
+INNER JOIN TotalCounts t
+    ON a.member_casual = t.member_casual
+GROUP BY a.member_casual, a.rideable_type, t.total_rides;
 
 
 -- 9. Average Ride Length of Riders with Annual Memberships using Classic, Docked, Electric Bikes 
